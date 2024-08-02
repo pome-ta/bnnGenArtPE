ab86951372eeaa9dd4e8b6edd6d10e76ddc90506
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-02 10:44:04 +0900

    background color

--- public/js/sketchDev.js
+++ public/js/sketchDev.js
@@ -10,7 +10,6 @@ const sketch = (p) => {
   p.setup = () => {
     // put setup code here
     p.ellipse(25, 25, 50, 50);
-    
   };
 
 };
@@ -19,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.title = title ? title : document.title;
   const canvasId = 'p5Canvas';
   const canvasTag = document.querySelector(`#${canvasId}`);
-  canvasTag.style.backgroundColor = 'gray';
+  canvasTag.style.backgroundColor = 'darkgray';
 
   canvasTag.addEventListener('touchmove', (e) => e.preventDefault(), {
     passive: false,
