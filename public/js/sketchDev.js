ff1fde1556f89e94f6ea57d9b969bb84a0496a7a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-05 23:40:09 +0900

    Update sketchDev.js

--- public/js/sketchDev.js
+++ public/js/sketchDev.js
@@ -1,4 +1,4 @@
-const title = '5.2 ノイズ・アニメーション';
+const title = '5.3.1 3 次元空間で描く';
 
 const sketch = (p) => {
   let w, h;
@@ -8,46 +8,17 @@ const sketch = (p) => {
 
   p.setup = () => {
     // put setup code here
-    p.createCanvas(500, 500);
+    p.createCanvas(500, 300, p.WEBGL);
     windowFlexSize();
-
-    p.background(0);
-    p.frameRate(24);
-
-    xstart = p.random(10);
-    ystart = p.random(10);
   };
+
   p.draw = () => {
-    p.background(0);
-
-    xstart += 0.01;
-    ystart += 0.01;
-
-    xnoise = xstart;
-    ynoise = ystart;
-
-    for (let y = 0; y <= h; y += 5) {
-      ynoise += 0.1;
-      xnoise = xstart;
-      for (let x = 0; x <= w; x += 5) {
-        xnoise += 0.1;
-        drawPoint(x, y, p.noise(xnoise, ynoise));
-      }
-    }
-  };
-
-  function drawPoint(x, y, noiseFactor) {
+    p.background(255);
     p.push();
-    p.translate(x, y);
-    p.rotate(noiseFactor * p.radians(540));
-    p.noStroke();
-    const edgeSize = noiseFactor * 35;
-    const grey = 150 + noiseFactor * 120;
-    const alph = 150 + noiseFactor * 120;
-    p.fill(grey, alph);
-    p.ellipse(0, 0, edgeSize, edgeSize / 2);
+    p.translate(0, 0, 300);
+    p.sphere(100, 9, 9);
     p.pop();
-  }
+  };
 
   function windowFlexSize(isFullSize = false) {
     const isInitialize =
@@ -90,4 +61,3 @@ document.addEventListener('DOMContentLoaded', () => {
   // --- start
   new p5(sketch, canvasId);
 });
-
