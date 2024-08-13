5159b9abd82ad9d47e15d53d982e56c575339a94
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-13 15:31:29 +0900

    Create 8_02_01_b.js

---
+++ public/js/fixSketches/8_02_01_b.js
@@ -0,0 +1,115 @@
+const title = '8.2.1 幹と枝';
+
+const sketch = (p) => {
+  let w, h;
+  let setupWidth, setupHeight, setupRatio;
+
+  let _numChildren = 3;
+  let _maxLevels = 3;
+  let _trunk;
+
+  class Branch {
+    #level;
+    #index;
+    #x;
+    #y;
+    #endx;
+    #endy;
+    #children = [];
+
+    constructor(lev, ind, ex, why) {
+      this.#level = lev;
+      this.#index = ind;
+      this.updateMe(ex, why);
+
+      if (this.#level < _maxLevels) {
+        this.#children = Array(_numChildren);
+        for (let x = 0; x < _numChildren; x++) {
+          this.#children[x] = new Branch(
+            this.#level + 1,
+            x,
+            this.#endx,
+            this.#endy
+          );
+        }
+      }
+    }
+
+    updateMe(ex, why) {
+      this.#x = ex;
+      this.#y = why;
+
+      this.#endx = this.#x + this.#level * (p.random(100) - 50 * setupRatio);
+      this.#endy = this.#y + 50 * setupRatio + this.#level * p.random(50);
+    }
+
+    drawMe() {
+      p.strokeWeight(_maxLevels - this.#level + 1);
+      p.line(this.#x, this.#y, this.#endx, this.#endy);
+      p.ellipse(this.#x, this.#y, 5 * setupRatio, 5 * setupRatio);
+      for (let i = 0; i < this.#children.length; i++) {
+        this.#children[i].drawMe();
+      }
+    }
+  }
+
+  p.setup = () => {
+    // put setup code here
+    const cnvs = p.createCanvas(750, 500);
+    windowFlexSize();
+
+    p.background(255);
+    p.noFill();
+    newTree();
+  };
+
+  p.draw = () => {
+    // put drawing code here
+  };
+
+  function newTree() {
+    _trunk = new Branch(1, 0, w / 2, 50 * setupRatio);
+    _trunk.drawMe();
+  }
+
+  function windowFlexSize(isFullSize = false) {
+    const isInitialize =
+      typeof setupWidth === 'undefined' || typeof setupHeight === 'undefined';
+    [setupWidth, setupHeight] = isInitialize
+      ? [p.width, p.height]
+      : [setupWidth, setupHeight];
+
+    const sizeRatio = 0.92;
+    const windowWidth = p.windowWidth * sizeRatio;
+    const windowHeight = p.windowHeight * sizeRatio;
+    if (isFullSize) {
+      w = windowWidth;
+      h = windowHeight;
+    } else {
+      const widthRatio =
+        windowWidth < setupWidth ? windowWidth / setupWidth : 1;
+      const heightRatio =
+        windowHeight < setupHeight ? windowHeight / setupHeight : 1;
+
+      setupRatio = Math.min(widthRatio, heightRatio);
+      w = setupWidth * setupRatio;
+      h = setupHeight * setupRatio;
+    }
+    p.resizeCanvas(w, h);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.title = title ? title : document.title;
+
+  const canvasId = 'p5Canvas';
+  const canvasTag = document.querySelector(`#${canvasId}`);
+  canvasTag.style.backgroundColor = 'darkgray';
+
+  canvasTag.addEventListener('touchmove', (e) => e.preventDefault(), {
+    passive: false,
+  });
+
+  // --- start
+  new p5(sketch, canvasId);
+});
