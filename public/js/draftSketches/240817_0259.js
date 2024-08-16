bc2bfc024912e9617b478a8ddee534fd91fba7b2
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-17 03:18:00 +0900

    up

--- public/js/draftSketches/240817_0259.js
+++ public/js/draftSketches/240817_0259.js
@@ -7,7 +7,7 @@ const sketch = (p) => {
   let setupWidth, setupHeight, setupRatio;
 
   let pentagon;
-  const _maxlevels = 3;
+  const _maxlevels = 5;
   const _corner = 5;
   let _strutFactor = 0.2;
 
@@ -29,8 +29,6 @@ const sketch = (p) => {
     }
   }
 
-
-
   class FractalRoot {
     #pointArr = [];
     #rootBranch;
@@ -47,7 +45,15 @@ const sketch = (p) => {
         const y = centY + _400 * p.sin(p.radians(ang));
         return new PointObj(x, y);
       });
-      
+      /*
+      let count = 0;
+      for (let i = 0; i < 360; i += 72) {
+        const x = centX + _400 * p.cos(p.radians(i));
+        const y = centY + _400 * p.sin(p.radians(i));
+        this.#pointArr[count] = new PointObj(x, y);
+        count++;
+      }
+      */
       this.#rootBranch = new Branch(0, 0, this.#pointArr);
     }
 
@@ -73,6 +79,9 @@ const sketch = (p) => {
 
       if (this.#level + 1 < _maxlevels) {
         const childBranch = new Branch(this.#level + 1, 0, this.#projPoints);
+        //this.#myBranches = [...this.#myBranches, childBranch];
+        //console.log(this.#myBranches)
+        //this.#myBranches = [childBranch];
 
         const lowerBranches = [...Array(this.#outerPoints.length).keys()].map(
           (k) => {
@@ -90,7 +99,22 @@ const sketch = (p) => {
         );
 
         this.#myBranches = [childBranch, ...lowerBranches];
-        
+        /*
+        for (let k = 0; k < this.#outerPoints.length; k++) {
+          let nextk = k - 1;
+          nextk += nextk < 0 ? this.#outerPoints.length : 0;
+          
+          const newPoints = [
+            this.#projPoints[k],
+            this.#midPoints[k],
+            this.#outerPoints[k],
+            this.#midPoints[nextk],
+            this.#projPoints[nextk],
+          ];
+          const childBranch = new Branch(this.#level + 1, k + 1, newPoints);
+          this.#myBranches = [...this.#myBranches, childBranch];
+        }
+        */
       }
     }
 
@@ -99,6 +123,13 @@ const sketch = (p) => {
       // draw outer shape
       const length = this.#outerPoints.length;
       for (let i = 0; i < length; i++) {
+        //let nexti = i + 1;
+        //nexti = nexti === this.#outerPoints.length ? 0 : nexti;
+        /*
+        if (nexti === this.#outerPoints.length) {
+          nexti = 0;
+        }
+        */
         const nexti = i + 1 === length ? 0 : i + 1;
         p.line(
           this.#outerPoints[i].x,
@@ -107,6 +138,10 @@ const sketch = (p) => {
           this.#outerPoints[nexti].y
         );
 
+        //[...Array(this.#myBranches.length)].forEach((_, k) => this.#myBranches[k].drawMe());
+
+        //this.#myBranches.forEach(branche => branche.drawMe());
+
         for (let k = 0; k < this.#myBranches.length; k++) {
           this.#myBranches[k].drawMe();
         }
@@ -114,7 +149,21 @@ const sketch = (p) => {
     }
 
     calcMidPoints() {
-      
+      /*
+      const mpArray = Array(this.#outerPoints.length);
+      for (let i = 0; i < this.#outerPoints.length; i++) {
+        let nexti = i + 1;
+        if (nexti === this.#outerPoints.length) {
+          nexti = 0;
+        }
+        const thisMP = this.calcMidPoint(
+          this.#outerPoints[i],
+          this.#outerPoints[nexti]
+        );
+        mpArray[i] = thisMP;
+      }
+      return mpArray;
+      */
       const length = this.#outerPoints.length;
       return [...Array(length).keys()].map(i => {
         const nexti = i + 1 === length ? 0 : i + 1;
@@ -123,6 +172,20 @@ const sketch = (p) => {
     }
 
     calcMidPoint(end1, end2) {
+      /*
+      let mx, my;
+      if (end1.x > end2.x) {
+        mx = end2.x + (end1.x - end2.x) / 2;
+      } else {
+        mx = end1.x + (end2.x - end1.x) / 2;
+      }
+      if (end1.y > end2.y) {
+        my = end2.y + (end1.y - end2.y) / 2;
+      } else {
+        my = end1.y + (end2.y - end1.y) / 2;
+      }
+      */
+
       const mx =
         end1.x > end2.x
           ? end2.x + (end1.x - end2.x) / 2
@@ -136,6 +199,21 @@ const sketch = (p) => {
     }
 
     calcStrutPoints() {
+      /*
+      const strutArray = Array(this.#midPoints.length);
+      for (let i = 0; i < this.#midPoints.length; i++) {
+        let nexti = i + 3;
+        if (nexti >= this.#midPoints.length) {
+          nexti -= this.#midPoints.length;
+        }
+        const thisSP = this.calcProjPoint(
+          this.#midPoints[i],
+          this.#outerPoints[nexti]
+        );
+        strutArray[i] = thisSP;
+      }
+      return strutArray;
+      */
       const length = this.#midPoints.length;
       return [...Array(length).keys()].map(i => {
         const nexti = i + 3 >= length ? i + 3 - length : i + 3;
@@ -144,11 +222,31 @@ const sketch = (p) => {
     }
 
     calcProjPoint(mp, op) {
-      const opp = op.x > mp.x ? op.x - mp.x : mp.x - op.x;
-      const adj = op.y > mp.y ? op.y - mp.y : mp.y - op.y;
-      const px = op.x > mp.x ? mp.x + opp * _strutFactor : mp.x - opp * _strutFactor;
-      const py = op.y > mp.y ? mp.y + adj * _strutFactor : mp.y - adj * _strutFactor;
-      
+      let px, py;
+      let adj, opp;
+      if (op.x > mp.x) {
+        opp = op.x - mp.x;
+      } else {
+        opp = mp.x - op.x;
+      }
+
+      if (op.y > mp.y) {
+        adj = op.y - mp.y;
+      } else {
+        adj = mp.y - op.y;
+      }
+
+      if (op.x > mp.x) {
+        px = mp.x + opp * _strutFactor;
+      } else {
+        px = mp.x - opp * _strutFactor;
+      }
+
+      if (op.y > mp.y) {
+        py = mp.y + adj * _strutFactor;
+      } else {
+        py = mp.y - adj * _strutFactor;
+      }
       return new PointObj(px, py);
     }
   }
@@ -212,3 +310,4 @@ document.addEventListener('DOMContentLoaded', () => {
   // --- start
   new p5(sketch, canvasId);
 });
+
